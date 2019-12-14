2fa98a01a385e3167108139ec940ad22eb181b0e
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-12-14 11:02:39 +0900

    initial commit

---
+++ lib/netrc.ex
@@ -0,0 +1,18 @@
+defmodule Netrc do
+  @moduledoc """
+  Documentation for Netrc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Netrc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/netrc_test.exs
@@ -0,0 +1,8 @@
+defmodule NetrcTest do
+  use ExUnit.Case
+  doctest Netrc
+
+  test "greets the world" do
+    assert Netrc.hello() == :world
+  end
+end
